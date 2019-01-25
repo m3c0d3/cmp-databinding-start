ed6568bbb474c1302827fb4e695b8c757794a609
m3c0d3 <m3c0d3@gmail.com>
2019-01-25 22:18:10 +0200

    64. Binding to custom events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,4 +12,26 @@ export class AppComponent {
     content: 'Just a test'
   }];
 
+  onAddServerAdded(serverData: {
+    serverName: string,
+    serverContent: string
+  }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onAddBlueprintAdded(blueprintData: {
+    serverName: string,
+    serverContent: string
+  }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
+
 }
